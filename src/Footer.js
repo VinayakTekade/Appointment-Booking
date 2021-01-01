6cb81124c1cc0498f430c51d89ab917bb4320de3
VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
2021-01-01 17:34:37 +0530

    Fixed Footer Component
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { Jumbotron, Container } from "reactstrap";
+import { Jumbotron, Container, Button } from "reactstrap";
 
 const Footer = (props) => {
   return (
     <div>
       <Jumbotron fluid>
         <Container fluid>
-          <h1 className="display-3">Fluid jumbotron</h1>
-          <p className="lead">
-            This is a modified jumbotron that occupies the entire horizontal
-            space of its parent.
-          </p>
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <h3 className="display-4 ">Schedule your meeting now</h3>
+            </div>
+            <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+              <Button color="primary" size="lg">
+                Book Appointment
+              </Button>
+            </div>
+          </div>
         </Container>
       </Jumbotron>
     </div>

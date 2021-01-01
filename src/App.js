85a5ba3f79d23396149b9a40683e876dd66d31d2
VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>
2021-01-01 17:07:56 +0530

    Added App Home Page Template
    Signed-off-by: VinayakTekade <39454332+VinayakTekade@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import "./assets/css/App.css";
+import NavBar from "./NavBar";
+import Greeting from "./Greeting";
+import Information from "./Information";
+import Footer from "./Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Greeting />
+      <Information />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Information.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Container } from "reactstrap";
+import doc1 from "./assets/img/doc1.jpg";
+import doc2 from "./assets/img/doc2.jpg";
+
+const Information = () => {
+  return (
+    <div>
+      <Container fluid>
+        <div className="row m-3 d-flex align-items-center">
+          <div className="col-12 col-md-6 p-5">
+            <img src={doc1} alt="" width="100%" height="auto" />
+          </div>
+          <div className="col-12 col-md-6 text-center p-5">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Laudantium atque fugiat ipsa accusamus? Rerum dolore aperiam nobis
+              harum! Quam blanditiis illum dolor impedit rem eum recusandae,
+              commodi quaerat quisquam ipsam doloremque expedita, praesentium
+              facere illo iste tempora eius asperiores natus pariatur quod.
+              Assumenda saepe sit ducimus iste, asperiores hic distinctio!
+            </p>
+          </div>
+        </div>
+        <div className="row m-3 d-flex align-items-center">
+          <div className="col-12 col-md-6 text-center p-5">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Laudantium atque fugiat ipsa accusamus? Rerum dolore aperiam nobis
+              harum! Quam blanditiis illum dolor impedit rem eum recusandae,
+              commodi quaerat quisquam ipsam doloremque expedita, praesentium
+              facere illo iste tempora eius asperiores natus pariatur quod.
+              Assumenda saepe sit ducimus iste, asperiores hic distinctio!
+            </p>
+          </div>
+          <div className="col-12 col-md-6 p-5">
+            <img src={doc2} alt="" width="100%" height="auto" />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Information;
